7942248c7525ad6f2f61783b5c7561e7b69309f0
saddydead1 <luckybeyhuk@gmail.com>
2024-07-01 10:49:11 +0300

    first commit

---
+++ src/main/kotlin/example/com/Application.kt
@@ -0,0 +1,12 @@
+package example.com
+
+import example.com.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
